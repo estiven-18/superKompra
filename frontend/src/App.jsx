a3d1d96bbe19fa895989c59bb56a52dfab45f8c5
estiven-18 <nelsoncuartas21@gmail.com>
2026-05-29 10:39:55 -0500

    inicio

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+const App = () => {
+  
+
+  return (
+    <>
+    <h1>Welcome to my React App!</h1>
+    <p>This is a simple React application.</p>
+    </>
+  )
+}
+
+export default App
