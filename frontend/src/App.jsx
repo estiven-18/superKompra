e85fb5fa04abb6c2a800bcc53d0bcb9d44aa51e4
estiven-18 <nelsoncuartas21@gmail.com>
2026-05-29 11:08:38 -0500

    video 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,16 @@
-import './App.css'
-import Home from './pages/Home'
-import Header from './layout/header'
-import Footer from './layout/Footer'
+import "./App.css";
 
+//import Home from "./pages/Home";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
 const App = () => {
-  
-
   return (
     <>
-    <h1>Welcome to my React App!</h1>
-    <p>This is a simple React application.</p>
-    <Header />
-    <Home />
-    <Footer />
+      <Header />
+      <main>contenido principal</main>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/layout/Footer.jsx
+++ frontend/src/layout/Footer.jsx
@@ -1,7 +1,21 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 const Footer = () => {
-    return ( <>
-    
-    </> );
-}
- 
+  const currentYear = new Date().getFullYear();
+  return (
+    <>
+      <footer>
+        <Container>
+          <Row>
+            <Col bg="dark" text="white">
+              SuperKompra
+            </Col>
+            <Col>&copy; {currentYear} SuperKompra. All rights reserved.</Col>
+          </Row>
+        </Container>
+      </footer>
+    </>
+  );
+};
+
 export default Footer;
